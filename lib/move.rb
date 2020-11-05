91a36913ddf0aecad8ad46fad2048fd3e364349c
Liz Burke <liz@veggiediner.com>
2020-11-05 16:16:34 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,13 +10,8 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-board = [" ", " ", " "]
-def update_array_at_with(array, index, value)
-  array[index] = value
-end
-
-update_array_at_with(board, 0, "X")
-
 def move(board, index, player)
-  board[index]=player
+  board[index] = player
   end
+
+def
