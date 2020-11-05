0ddaf145df393793f96b30ca9ba7023772c33c1b
Liz Burke <liz@veggiediner.com>
2020-11-05 15:39:05 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -13,10 +13,3 @@ end
 def move(board, index, player)
   board[index] = player
 end
-
-board = ["","",""]
-def update_array_at_with(array, index, value)
-array[index] = value
-end
-
-update_array_at_with(board, 0, "X")
